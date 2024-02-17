87ca138a57c57e6e4394cd40b757b49cd0b2185f
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2024-02-17 19:21:44 -0300

    feat: created unit test to customer not found exception

--- src/usecase/customer/find/find.customer.unit.spec.ts
+++ src/usecase/customer/find/find.customer.unit.spec.ts
@@ -1,6 +1,7 @@
 import Customer from "../../../domain/customer/entity/customer";
 import AddressVO from "../../../domain/customer/dto/addressVO";
 import FindCustomerUseCase from "./find.customer.usecase";
+import * as inspector from "inspector";
 
 const customer = new Customer("123", "John");
 const address = new AddressVO("Street", 123, "Zip", "City")
@@ -44,5 +45,23 @@ describe("Test find customer use case", () => {
         const result = await usecase.execute(input);
 
         expect(result).toEqual(output);
+    });
+
+    it("should not find customer", async () => {
+        const customerRepository = MockRepository()
+        customerRepository.find.mockImplementation(() => {
+            throw new Error("Customer not found")
+        })
+        const usecase = new FindCustomerUseCase(customerRepository)
+
+        const input = {
+            id: "123",
+        }
+
+        await expect(
+            () => {
+                return usecase.execute(input);
+            }
+        ).rejects.toThrow("Customer not found");
     });
 })
