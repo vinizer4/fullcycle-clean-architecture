820d16036fd96efcb0956eba33f1809432495640
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2024-02-18 19:37:47 -0300

    feat: created list customer usecase and unit tests

---
+++ src/usecase/customer/list/list.customer.unit.spec.ts
@@ -0,0 +1,35 @@
+import CustomerFactory from "../../../domain/customer/factory/customer.factory";
+import AddressVO from "../../../domain/customer/dto/addressVO";
+import ListCustomerUseCase from "./list.customer.usecase";
+
+const address1 = new AddressVO("Street 1", 1, "Zip", "City")
+const address2 = new AddressVO("Street 2", 2, "Zip", "City")
+const customer1 = CustomerFactory.createWithAddress("Customer 1", address1)
+const customer2 = CustomerFactory.createWithAddress("Customer 2", address2)
+
+const MockRepository = () => {
+    return {
+        create: jest.fn(),
+        update: jest.fn(),
+        delete: jest.fn(),
+        find: jest.fn(),
+        findAll: jest.fn().mockResolvedValue(Promise.resolve([customer1, customer2]))
+    }
+}
+
+describe("Unit test for list customer use case", () => {
+
+    it("should list a customers", async () => {
+        const repository = MockRepository()
+        const usecase = new ListCustomerUseCase(repository)
+
+        const output = await usecase.execute({})
+
+        expect(output.customers.length).toBe(2)
+        expect(output.customers[0].name).toBe(customer1.name)
+        expect(output.customers[1].name).toBe(customer2.name)
+        expect(output.customers[0].Address.street).toBe(customer1.Address.street)
+        expect(output.customers[1].Address.street).toBe(customer2.Address.street)
+        expect(output.customers.length).toBe(2)
+    })
+})
