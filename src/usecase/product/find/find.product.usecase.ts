49b974a64073f07d8a2d676295b08ce68cbf1833
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2024-02-18 19:47:13 -0300

    feat: implemented find product usecase

---
+++ src/usecase/product/find/find.product.usecase.ts
@@ -0,0 +1,20 @@
+import { InputFindProductDto, OutputFindProductDto } from "./find.product.dto";
+import ProductRepositoryInterface from "../../../domain/product/repository/product/product.repository.interface";
+
+export default class FindProductUseCase {
+    private productRepository: ProductRepositoryInterface;
+
+    constructor(productRepository: ProductRepositoryInterface) {
+        this.productRepository = productRepository;
+    }
+
+    async execute(input: InputFindProductDto): Promise<OutputFindProductDto> {
+        const product = await this.productRepository.find(input.id);
+
+        return {
+            id: product.id,
+            name: product.name,
+            price: product.price,
+        };
+    }
+}
