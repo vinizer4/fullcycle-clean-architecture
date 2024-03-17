b739df46a5d7c6309d10967b2a445c68e6879b26
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2024-03-17 16:18:54 -0300

    feat: implemented route to list all customers endpoint

--- src/infra/web/api/express/__tests__/customer.e2e.spec.ts
+++ src/infra/web/api/express/__tests__/customer.e2e.spec.ts
@@ -41,7 +41,7 @@ describe("E2E test for customer", () => {
         expect(response.status).toBe(500);
     })
 
-    it("should list all customers", async () => {
+    it("should list all customer", async () => {
         const response = await request(app)
             .post("/customer")
             .send({
@@ -53,9 +53,7 @@ describe("E2E test for customer", () => {
                     zip: "12345",
                 },
             });
-
         expect(response.status).toBe(200);
-
         const response2 = await request(app)
             .post("/customer")
             .send({
@@ -67,31 +65,17 @@ describe("E2E test for customer", () => {
                     zip: "12344",
                 },
             });
-
         expect(response2.status).toBe(200);
 
-
-        const listResponse = await request(app)
-            .get("/customer")
-            .send();
+        const listResponse = await request(app).get("/customer").send();
 
         expect(listResponse.status).toBe(200);
-        expect(listResponse.body.length).toBe(2);
-
-        const customer = listResponse.body[0];
+        expect(listResponse.body.customers.length).toBe(2);
+        const customer = listResponse.body.customers[0];
         expect(customer.name).toBe("John");
         expect(customer.address.street).toBe("Street");
-        expect(customer.address.city).toBe("City");
-        expect(customer.address.number).toBe(123);
-        expect(customer.address.zip).toBe("12345");
-
-        const customer2 = listResponse.body[1];
+        const customer2 = listResponse.body.customers[1];
         expect(customer2.name).toBe("Jane");
         expect(customer2.address.street).toBe("Street 2");
-        expect(customer2.address.city).toBe("City 2");
-        expect(customer2.address.number).toBe(1234);
-        expect(customer2.address.zip).toBe("12344");
-
-        }
-    )
+    });
 });
